8c725c709b43af0ff237a7714c8e82651e29b269
Jayashankar <jayashankarkanigiri@gmail.com>
2023-11-14 09:50:02 +0530

    introduced a player to the game. When clicking on a square, show either X or 0

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,21 +3,30 @@ import Square from './Square';
 
 const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
-  console.log(squares);
+  const [isXNext, setIsXNext] = useState(false);
 
-  const handleSquareClick = (clickedPosition) => {
-    setSquares((currentSquares) => {
+  const handleSquareClick = clickedPosition => {
+     // null, 'X', '0'
+
+    if(squares[clickedPosition]) {
+      return;
+    }
+
+
+    setSquares(currentSquares => {
       return currentSquares.map((squareValue, position) => {
         if (clickedPosition === position) {
-          return 'X';
+          return isXNext ? 'X' : '0';
         } 
           return squareValue;
         
       });
     });
+
+    setIsXNext((currentIsXNext) => !currentIsXNext);
   };
 
-  const renderSquare = (position) => {
+  const renderSquare = position => {
     return (
       <Square
         value={squares[position]}
